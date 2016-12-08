90292d2c1be42c8195d040b34ffc888022ba688b
haker20154 <behrang.parsa94@gmail.com>
2016-12-08 23:02:23 +0330

    Update ver.lua

--- plugins/ver.lua
+++ plugins/ver.lua
@@ -18,21 +18,21 @@ local uptime = kham:split(",")[1]
 local admins = admin_list(msg)
 local data = load_data(_config.moderation.data)
 local group_link = data[tostring(1065958052)]['settings']['set_link']
-local channel = '@CH_NEWS'
-local owner = '@IT_MKH'
+local channel = '@Space_Team'
+local owner = '@Boy_virtual'
 local htp = https.request('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160119T111342Z.fd6bf13b3590838f.6ce9d8cca4672f0ed24f649c1b502789c9f4687a&format=plain&lang=fa&text='..uptime)
 local data = json:decode(htp)
 local up = data.text[1]
-local privator = '@MKH_PV_BOT'
+local privator = '@Boy_Virtual_Bot'
 local space = '______________________________'
 if not group_link then
-group_link = "Unset"
+group_link = "https://telegram.me/joinchat/DFuJfj9tn_wnDj5ks5ARFA"
 end
-return "#Cruel_Bot\n\n⏱مدت زمان روشن بودن :\n"..up.."\n"..space.."\n💢 نویسنده و صاحب امتیاز ربات : "..owner.."\n"..admins.."\n"..space.."\n\n📢 کانال رسمی اخبار تیم کرول : "..channel.."\n\n🤖ربات پیام رسان برای ارتباط باما (برای افراد ریپورت) : "..privator.."\n\n 🔰لینک ساپورت🔰 :\n"..group_link
+return "#Ultra_Space\n\n⏱مدت زمان روشن بودن :\n"..up.."\n"..space.."\n💢 نویسنده و صاحب امتیاز ربات : "..owner.."\n"..admins.."\n"..space.."\n\n📢 کانال رسمی اخبار تیم کرول : "..channel.."\n\n🤖ربات پیام رسان برای ارتباط باما (برای افراد ریپورت) : "..privator.."\n\n 🔰لینک ساپورت🔰 :\n"..group_link
 end
 return {
 patterns = {
-"^[!/#][Cc][Rr][Uu][Ee][Ll]$",
+"^[!/#][Ss][Pp][Aa][Cc][Ee]$",
 },
 run = run
 }
